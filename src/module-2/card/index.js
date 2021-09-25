7ef593dbaea6b97d4fa14fdcb3abfae09f07715a
alexandrpedenko <alexandrpedenko@gmail.com>
2021-09-25 16:59:19 +0300

    Done module-2 and module-3 tasks

--- src/module-2/card/index.js
+++ src/module-2/card/index.js
@@ -1,14 +1,14 @@
 export default class Card {
   element;
 
-  constructor ({
-    id = '',
+  constructor({
+    id = "",
     images = [],
-    title = '',
+    title = "",
     rating = 0,
     price = 0,
-    category = '',
-    brand = ''
+    category = "",
+    brand = "",
   } = {}) {
     this.id = id;
     this.images = images;
@@ -21,7 +21,52 @@ export default class Card {
     this.render();
   }
 
-  render () {
-    // ... your logic
+  render() {
+    const cardWrapper = document.createElement("div");
+    cardWrapper.setAttribute("data-element", "body");
+    cardWrapper.classList.add("product-wrapper");
+    cardWrapper.innerHTML = this.buildCard();
+
+    this.element = cardWrapper;
+  }
+
+  buildCard() {
+    return `
+      <div class="product">
+        <div class="product__image-wrapper">
+          <img class="product__image"
+            src="${this.images[0]}"
+            alt="${this.title}"
+          />
+        </div>
+        <div class="product__rating-price">
+          <div class="product__rating">
+            <span class="product__rating-text">${this.rating}</span>
+          </div>
+          <span class="product__price">${this.price}₴</span>
+        </div>
+        <div class="product__info">
+          <h3 class="product__name">${this.title}r</h3>
+          <span class="product__description">
+          ${this.category} | ${this.brand}
+          </span>
+        </div>
+        <div class="product__buttons">
+          <button class="product__button button button_secondary button_medium">
+            <span class="product__wishlist">Wishlist</span>
+          </button>
+          <button class="product__button button button_primary button_medium">
+            <span class="product__addToCart">Add to cart</span>
+          </button>
+        </div>
+      </div>
+    `;
+  }
+
+  destroy() {
+    if (this.element) {
+      this.element.remove();
+      this.element = null;
+    }
   }
 }
--- src/module-3/cards-list-v1/index.js
+++ src/module-3/cards-list-v1/index.js
@@ -1,9 +1,47 @@
-import Card from '../../module-2/card/index.js';
+import Card from "../../module-2/card/index.js";
 
 export default class CardsList {
-  constructor (data = []) {
-    this.data = data;
+  element;
 
-    // ... your logic
+  constructor(data = [], Card) {
+    this.data = data;
+    this.card = Card;
+
+    this.render();
+  }
+
+  render() {
+    const listWrapper = document.createElement("div");
+    listWrapper.classList.add("product-list");
+
+    this.element = listWrapper;
+    this.buildCardsLits(this.element, this.data);
+  }
+
+  buildCardsLits(wrapper, cardsData) {
+    if (cardsData.length < 1) return;
+
+    Array.from(cardsData).forEach((item) => {
+      let { element } = new Card(item);
+
+      if (element) {
+        wrapper.append(element);
+      }
+    });
+  }
+
+  update(updateData) {
+    if (!this.element) return;
+    this.element.innerHTML = "";
+    this.data = updateData;
+
+    this.buildCardsLits(this.element, this.data);
+  }
+
+  destroy() {
+    if (this.element) {
+      this.element.remove();
+      this.element = null;
+    }
   }
 }
